326f6b43f45b50a6819bee39f21b934baf96e139
smkclms <smkn1cilimus@gmail.com>
2025-12-27 19:23:31 +0700

    default pw guru

--- application/models/Guru_model.php
+++ application/models/Guru_model.php
@@ -30,7 +30,7 @@ class Guru_model extends CI_Model {
     // insert user guru (DEFAULT)
     $user = [
         'username' => $data['email'],
-        'password' => password_hash('@Smkn1cilimus', PASSWORD_DEFAULT),
+        'password' => password_hash('KOSONG', PASSWORD_DEFAULT),//masukin password default utk guru
         'nama'     => $data['nama'],
         'email'    => $data['email'],
         'role_id'  => 3, // GURU
